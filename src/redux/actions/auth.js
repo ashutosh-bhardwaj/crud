5d8270f183b7fb0c6c6f95d80d92e32556837e1e
Ashutosh <ashutsh.bhardwaj@daffodilsw.com>
2018-06-18 17:15:20 +0530

    Update actions

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -1,7 +1,30 @@
-export const logIn = () => ({
-  type: 'LOGIN'
-})
+//----------------
+// Constants
+//----------------
+export const LOGIN = 'LOGIN';
+export const LOGOUT = 'LOGOUT';
 
-export const logOut = () => ({
-  type: 'LOGOUT'
-})
+//----------------
+// Action creators
+//----------------
+
+/**
+ * @export function logIn
+ * @returns {object} action with logs in the user
+ */
+export function logIn() {
+  return {
+    type: LOGIN,
+  };
+}
+
+/**
+ * @export function logOut
+ * @returns {object} action with logs out the user
+ */
+export function logOut() {
+  return {
+    type: LOGOUT,
+  };
+}
+
--- src/redux/actions/phone.js
+++ src/redux/actions/phone.js
@@ -1,18 +1,49 @@
+//----------------
+// Constants
+//----------------
+export const ADD_PHONE = 'ADD_PHONE';
+export const REMOVE_PHONE = 'REMOVE_PHONE';
+export const UPDATE_PHONE = 'UPDATE_PHONE';
+
 let nextPhone = 0;
 
-export const addPhone = payload => ({
-  type: "ADD_PHONE",
-  id: nextPhone++,
-  payload
-});
+//----------------
+// Action creators
+//----------------
 
+/**
+ * @exports function addPhone
+ * @param {object} payload
+ * @returns {object} action which add a phone to store
+ */
+export function addPhone(payload) {
+  return {
+    type: 'ADD_PHONE',
+    id: nextPhone++,
+    payload,
+  };
+}
+
+
+/**
+ * @exports function remove
+ * @param {string} id
+ * @returns {object} action which remove a phone with the `id` from store
+ */
 export const removePhone = id => ({
-  type: "REMOVE_PHONE",
-  id
+  type: 'REMOVE_PHONE',
+  id,
 });
 
+
+/**
+ * @exports function updatePhone
+ * @param {object} payload
+ * @param {string} id
+ * @returns {object} action which update the phone with `id` in store
+ */
 export const updatePhone = (id, payload) => ({
-  type: "UPDATE_PHONE",
+  type: 'UPDATE_PHONE',
   id,
-  payload
+  payload,
 });
