09453126a6be617b3705e539cdc7b86e55a9f787
Ashutosh <ashutsh.bhardwaj@daffodilsw.com>
2018-06-19 11:09:26 +0530

    Update with '@material-ui/core'

--- src/views/Phones/PhoneDetail.jsx
+++ src/views/Phones/PhoneDetail.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import {
-  Card,
-  CardActions,
-  CardMedia,
-  CardTitle,
-  CardText,
-} from 'material-ui/Card';
-import RaisedButton from 'material-ui/RaisedButton';
+import PropTypes from 'prop-types';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 import { removePhone } from '../../redux/actions';
 import { notify } from '../../redux/actions/notice';
@@ -25,17 +25,17 @@ class PhoneDetail extends Component {
 
   componentWillMount() {
     const { match, phones } = this.props;
-    const id = match.params.id;
+    const { id } = match.params;
     const selectedPhone = phones.filter(phone => phone.id === id * 1).pop();
 
     this.setState({ phone: selectedPhone });
   }
 
   handleClickOnDelete() {
-    const { removePhone, history, notify } = this.props;
+    const { history } = this.props;
     const { id } = this.state.phone;
-    removePhone(id);
-    notify({ message: 'Deleted Successfully!' });
+    this.props.removePhone(id);
+    this.props.notify({ message: 'Deleted Successfully!' });
     history.push(endPoints.default);
   }
 
@@ -48,36 +48,59 @@ class PhoneDetail extends Component {
   render() {
     const { model, price, status } = this.state.phone;
     return (
-      <Card>
-        <CardMedia>
-          <img
-            src={PHONE}
-            alt="iphone"
-            style={{ height: '100%' }}
-          />
-        </CardMedia>
-        <CardTitle title={model} />
-        <CardText>$ {price}</CardText>
-        <CardText>${`It's an ${status} iphone`}</CardText>
-
-        <CardActions>
-          <RaisedButton
-            label="Edit"
-            backgroundColor="#2ecc71"
-            labelColor="#fff"
-            onClick={this.handleClickOnEdit}
-          />
-          <RaisedButton
-            label="Delete"
-            backgroundColor="#c0392b"
-            labelColor="#fff"
-            onClick={this.handleClickOnDelete}
-          />
-        </CardActions>
-      </Card>
+      <div style={{ flexGrow: 1 }}>
+        <Grid container justify="center">
+          <Grid item xs={12} sm={6}>
+            <Card style={{ maxWidth: '414px' }}>
+              <CardMedia>
+                <img
+                  src={PHONE}
+                  alt="iphone"
+                  style={{ height: '50%', width: '100%' }}
+                />
+              </CardMedia>
+              <CardContent>
+                <Typography component="h2" color="primary" variant="title" gutterBottom>
+                  {model}
+                </Typography>
+                <Typography component="h3" color="secondary" variant="subheading" gutterBottom>
+                  $ {price}
+                </Typography>
+                <Typography component="p" color="textSecondary" variant="subheading" gutterBottom>
+                  {`It's an ${status} iphone`}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button
+                  variant="raised"
+                  color="secondary"
+                  onClick={this.handleClickOnEdit}
+                >
+                  Edit
+                </Button>
+                <Button
+                  variant="raised"
+                  color="primary"
+                  onClick={this.handleClickOnDelete}
+                >
+                  Delete
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        </Grid>
+      </div>
     );
   }
 }
+
+PhoneDetail.propTypes = {
+  match: PropTypes.object.isRequired,
+  phones: PropTypes.array.isRequired,
+  removePhone: PropTypes.func.isRequired,
+  notify: PropTypes.func.isRequired,
+  history: PropTypes.object.isRequired,
+};
 
 const PhoneDetailWithRouter = withRouter(PhoneDetail);
 
