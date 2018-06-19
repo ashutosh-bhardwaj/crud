ee5dfdd6be036daea8b75cdc106f642980c877d9
Ashutosh <ashutsh.bhardwaj@daffodilsw.com>
2018-06-19 11:07:45 +0530

    Update fixed indentation

--- src/config/routes/routes.js
+++ src/config/routes/routes.js
@@ -5,55 +5,55 @@ import { LoginRequired } from '../../components/RouteWrappers';
 import endPoints from './endpoints';
 import { HeaderLayout } from '../../views/Layouts';
 import {
-    PhoneList,
-    PhoneDetail,
-    AddPhone,
-    EditPhone
+  PhoneList,
+  PhoneDetail,
+  AddPhone,
+  EditPhone,
 } from '../../views/Phones';
 
 export default [
-    {
-        component: App,
+  {
+    component: App,
+    routes: [
+      {
+        path: endPoints.signin,
+        exact: true,
+        component: AuthSigin, // View component
+      },
+      {
+        path: endPoints.default,
+        component: LoginRequired, // Route protector
         routes: [
-            {
+          {
+            component: HeaderLayout,
+            routes: [
+              {
                 path: endPoints.signin,
                 exact: true,
                 component: AuthSigin, // View component
-            },
-            {
+              },
+              {
                 path: endPoints.default,
-                component: LoginRequired,   // Route protector
-                routes: [
-                    {
-                        component: HeaderLayout,
-                        routes: [
-                            {
-                                path: endPoints.signin,
-                                exact: true,
-                                component: AuthSigin, // View component
-                            },
-                            {
-                                path: endPoints.default,
-                                exact: true,
-                                component: PhoneList, // View component
-                            },
-                            {
-                                path: endPoints.phoneAdd,
-                                exact: true,
-                                component: AddPhone // View component
-                            },
-                            {
-                                path: endPoints.phoneEditWithId,
-                                component: EditPhone // View component
-                            },
-                            {
-                                path: endPoints.phoneDetailWithId,
-                                component: PhoneDetail // View component
-                            },
-                        ]
-                    }
-                ]
-            }
-        ]
-    }
+                exact: true,
+                component: PhoneList, // View component
+              },
+              {
+                path: endPoints.phoneAdd,
+                exact: true,
+                component: AddPhone, // View component
+              },
+              {
+                path: endPoints.phoneEditWithId,
+                component: EditPhone, // View component
+              },
+              {
+                path: endPoints.phoneDetailWithId,
+                component: PhoneDetail, // View component
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
 ];
